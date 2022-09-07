9a15c77c2bea1714046a25facc18588bb3f3cde9
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-09-07 16:25:52 -0400

    complete thought and profile pages with reaction and friend lists

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,19 +1,46 @@
-import React from 'react';
+// modules
+import React from "react";
+import { useParams } from "react-router-dom";
 
-const SingleThought = props => {
+import { useQuery } from "@apollo/client";
+import { QUERY_THOUGHT } from "../utils/queries";
+
+// reaction component
+import ReactionList from "../components/ReactionList";
+
+const SingleThought = (props) => {
+  const { id: thoughtId } = useParams();
+
+  // query for the single though selected
+  const { loading, data } = useQuery(QUERY_THOUGHT, {
+    variables: { id: thoughtId },
+  });
+
+  const thought = data?.thought || {};
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  // return JSX
   return (
     <div>
       <div className="card mb-3">
         <p className="card-header">
           <span style={{ fontWeight: 700 }} className="text-light">
-            Username
-          </span>{' '}
-          thought on createdAt
+            {thought.username}
+          </span>{" "}
+          thought on {thought.createdAt}
         </p>
         <div className="card-body">
-          <p>Thought Text</p>
+          <p>{thought.thoughtText}</p>
         </div>
       </div>
+
+      {/* reaction component, passing reactions array as a prop */}
+      {thought.reactionCount > 0 && (
+        <ReactionList reactions={thought.reactions} />
+      )}
     </div>
   );
 };
